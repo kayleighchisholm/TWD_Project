3b13bdedd59997672034291d0cd21cbb343c03b3
Kayleigh Chisholm <2349784c@student.gla.ac.uk>
2018-02-10 09:33:47 +0000

    added max_length attribute

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    max_length = 128
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -20,7 +20,7 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=Category.max_length)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
